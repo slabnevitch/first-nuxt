01cf90f44a29d5898df3185e11c522266d00b7b3
slabnevitch <handofschpilrain@mail.ru>
2021-06-20 20:57:05 +0500

    add repository name

---
+++ _nuxt/static/1624204491/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffirst-nuxt\u002F_nuxt\u002Fstatic\u002F1624204491",layout:"default",error:a,state:{users:{users:[]}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffirst-nuxt\u002F",assetsPath:"\u002Ffirst-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
